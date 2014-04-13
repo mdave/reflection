39d219a44c902c3eb45e7d4bfa827c59283308d9
David Moxey <dave@xyloid.org>
2014-04-13 11:35:21 +0100

    Change URL

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@ $year = ($yeartmp = get_opt_or_default('copyright_year')) ? $yeartmp : date('Y')
 ?>
 
 <div id="footer">
-Powered by <a href="http://wordpress.org/">WordPress</a>, <a href="http://xyloid.org/projects/reflection/">Reflection <?=$vnum?></a> 
+Powered by <a href="http://wordpress.org/">WordPress</a>, <a href="http://github.com/mdave/reflection/">Reflection <?=$vnum?></a> 
 and <a href="http://johannes.jarolim.com/yapb/">YAPB</a>: <a href="#">Entries (RSS)</a> - <a href="#">Comments (RSS)</a>.<br />
 
 <?php if ($copy):?>&copy; <?=$copy?> <?=$year?>.<?php endif;?>
